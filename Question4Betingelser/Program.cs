b2ad81374bae2af61863cbefe24152b1c1a17d78
DNIIBOY <daniel@nettelfield.com>
2023-06-01 13:21:45 +0200

    Big beauty q4

--- Question4Betingelser/Program.cs
+++ Question4Betingelser/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Værdi 1: ");
+int val1 = int.Parse(Console.ReadLine());
+Console.Write("Værdi 2: ");
+int val2 = int.Parse(Console.ReadLine());
+
+int min = val1 < val2 ? val1 : val2;
+int max = val1 > val2 ? val1 : val2;
+Console.WriteLine($"Maximum: {max}");
+
+List<int> numbers = new();
+for (int i = 0; i < min; i++){
+    numbers.Add(i);
+}
+
+Console.WriteLine(numbers);
+foreach (var num in numbers){
+    Console.WriteLine(num);
+}
